987172629f90137eb99d246ba36a48ec5e72aca9
ti2 <lagarde.pa@gmail.com>
2019-04-18 16:41:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "simpleemergencystop"
+plugin_identifier = "drawingapp"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_simpleemergencystop"
+plugin_package = "octoprint_drawingapp"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-SimpleEmergencyStop"
+plugin_name = "OctoPrint-DrawingApp"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.2.5"
@@ -21,13 +21,13 @@ plugin_version = "0.2.5"
 plugin_description = """A simple plugin that add an emergency stop buton on NavBar"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Clément Sébastien"
+plugin_author = ""
 
 # The plugin's author's mail address.
-plugin_author_email = "seb6596@gmail.com"
+plugin_author_email = ""
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/BrokenFire/OctoPrint-SimpleEmergencyStop"
+plugin_url = "https://github.com/BrokenFire/OctoPrint-DrawingApp"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
