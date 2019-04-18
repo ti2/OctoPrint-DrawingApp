05a5fec137d23f1190bfc389573bc54a48a5678a
ti2 <lagarde.pa@gmail.com>
2019-04-18 16:42:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.2.5"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """A simple plugin that add an emergency stop buton on NavBar"""
+plugin_description = """A drawing app"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = ""
